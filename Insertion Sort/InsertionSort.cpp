b1acc2a3a4fa5cd755d57b9f8c0001c57070235c
Khang Hoang <khangben367@gmail.com>
2024-12-29 09:08:09 -0800

    changes to main and added functions

--- Insertion Sort/InsertionSort.cpp
+++ Insertion Sort/InsertionSort.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int main()
+
+
+void insertionSort(int arr[], int size)
 {
   
+
+
+
+}
+
+
+void printArray(int arr[], int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  /*
+  int size; 
+  cout << "How many elements in the array: ";
+  cin >> size; 
+  cout << endl;
+
+  if (size == 0)
+  {
+    cout << "Empty Array" << endl;
+    return 0;
+  }
+  
+  int array[size];
+
+  cout << "Enter " << size << " elements: " << endl;
+  for(int i = 0; i < size; i++)
+  {
+    cin >> array[i];
+  }
+
+    if (size <= 1)
+  {
+    cout << "Arary is already sorted: ";
+    printArray(array, size);
+  }
+
+  */
+  
+  int size = 5; 
+  int array[] = {7, 12, 9, 11, 3};
+
+  cout << "Unsorted Array: ";
+  printArray(array, size);
+
+
+
 
 
   return 0;
