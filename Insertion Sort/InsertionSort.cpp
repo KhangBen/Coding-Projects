046363a3609a62549d91b2b6427d9246af60afb9
Khang Hoang <khangben367@gmail.com>
2024-12-29 18:30:10 -0800

    added insertionsort function

--- Insertion Sort/InsertionSort.cpp
+++ Insertion Sort/InsertionSort.cpp
@@ -5,10 +5,31 @@ using namespace std;
 
 void insertionSort(int arr[], int size)
 {
-  
+  // assume first element is already sorted i.e arr[0]
+  for(int i = 1; i < size; i++)
+  {
+    int key = arr[i]; // to store element
+    int j = i - 1; // get the previous index
+    while(arr[j] > arr[i]) // change ending boundary
+    {
+      // perform operation
+      arr[j+1] = arr[j];
+      j--; 
+    }
 
 
 
+  }
+
+
+
+}
+
+void swap(int& a, int& b)
+{
+  int temp = a; 
+  a = b; 
+  b = a; 
 }
 
 
